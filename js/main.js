087976d9bd2aaf5176799ed358e771907ab7e58e
iutria <ivanutria.17@gmail.com>
2022-11-06 01:06:21 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,54 @@
+
+const urlWhatsapp = 'https://web.whatsapp.com/';
+const urlFacebook = 'https://www.facebook.com/globaltiz';
+const urlInstagram = 'https://www.instagram.com/globaltiz_it';
+
+const paginas = `
+<ul class="nav justify-content-end">
+    <li class="nav-item">
+        <a class="nav-link" href="#que-hecemos">¿Que hacemos?</a> 
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#clientes">Clientes</a>
+    </li>
+    <li class="nav-item">
+        <a class="nav-link" href="#contactanos">Contactanos</a>
+    </li>
+</ul>
+`;
+
+const redes = `
+    <a href="${urlWhatsapp}" target="_blank"><i class="bi bi-whatsapp"></i></a>
+    <a href="${urlFacebook}" target="_blank"><i class="bi bi-facebook"></i></a>
+    <a href="${urlInstagram}" target="_blank"><i class="bi bi-instagram"></i></a>
+`;
+
+window.addEventListener("scroll",function(){
+    
+    document.querySelector('nav').style.background = window.pageYOffset >= 50 ? '#0577BE': 'transparent';
+    
+    const red  = document.getElementById('redes');
+
+    if(window.pageYOffset>5800){
+        red.style.right = '-100px'
+    }else{
+        red.style.right = '0px';
+    }
+
+    
+})
+
+/**
+ * cargar el menu
+ */
+const offcanvasbody = document.querySelector('.offcanvas-body');
+const menu = document.getElementById('menu');
+offcanvasbody.innerHTML = paginas;
+menu.innerHTML = paginas;
+
+
+/**
+ * cargar redes
+ */
+const redesVista = document.querySelector('.redes');
+redesVista.innerHTML = redes;
