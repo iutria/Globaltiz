d0720682c92713f58579698b56add53b8a8c05a7
iutria <ivanutria.17@gmail.com>
2022-11-16 14:59:52 -0500

    mismo texto corregido

--- js/lenguajes.js
+++ js/lenguajes.js
@@ -41,27 +41,22 @@ const otros = [
     ],
     [
         'analitica-de-datos',
-        `<li>Product inventory</li>
-        <li>Product categorization</li>
-        <li>Payment gateways</li>
-        <li>Shipment notification</li>
-        <li>Product discounts promotions</li>
-        <li>Coupons & Promo Codes</li>
-        <li>Billing</li>
-        <li>Shopping cart</li>
-        <li>Webchat</li>`
+        `<li>Big data</li>
+        <li>Machine learning</li>
+        <li>Data Quality</li>
+        <li>Real-time data movement</li>`
     ],
     [
         'cloud-computing',
-        `<li>Product inventory</li>
-        <li>Product categorization</li>
-        <li>Payment gateways</li>
-        <li>Shipment notification</li>
-        <li>Product discounts promotions</li>
-        <li>Coupons & Promo Codes</li>
-        <li>Billing</li>
-        <li>Shopping cart</li>
-        <li>Webchat</li>`
+        `<li>Amazon Web Services - AWS</li>
+        <li>Oracle Cloud Infrastructure - OCI</li>
+        <li>Cloud Migration</li>
+        <li>Cloud architecture design</li>
+        <li>Time to market</li>
+        <li>Cost optimization</li>
+        <li>Security</li>
+        <li>Low-latency platforms</li>`
+
     ],
 ];
 
